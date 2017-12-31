5ca82cc7327c27103fe86e2248b1045b7d3a50c6
James Netherton <jamesnetherton@gmail.com>
2017-12-31 14:27:42 +0000

    Add missing validation to some commands

--- cmd/light_brightness_set.go
+++ cmd/light_brightness_set.go
@@ -1,6 +1,10 @@
 package cmd
 
-import "github.com/jamesnetherton/homehub-cli/service"
+import (
+	"errors"
+
+	"github.com/jamesnetherton/homehub-cli/service"
+)
 
 // NewLightBrightnessSetCommand creates a new command to invoke the Hub LightBrightnessSet function
 func NewLightBrightnessSetCommand(authenticatingCommand *GenericCommand) *AuthenticationRequiringCommand {
@@ -8,8 +12,16 @@ func NewLightBrightnessSetCommand(authenticatingCommand *GenericCommand) *Authen
 		GenericCommand: GenericCommand{
 			Name:        "LightBrightnessSet",
 			Description: "Sets the Home Hub LED brightness percentage value",
+			ArgNames:    []string{"brightness"},
+			ArgTypes:    []string{"int"},
 			Exec: func(context *CommandContext) {
-				brightness, _ := context.GetIntArg(0)
+				brightness, err := context.GetIntArg(0)
+				if err != nil || brightness <= 0 || brightness > 100 {
+					parseErr := errors.New("Brightness must be a numeric value between 0 and 100")
+					context.SetResult(nil, parseErr)
+					return
+				}
+
 				context.SetResult(nil, service.GetHub().LightBrightnessSet(brightness))
 			},
 		},
--- cmd/light_enable.go
+++ cmd/light_enable.go
@@ -1,6 +1,10 @@
 package cmd
 
-import "github.com/jamesnetherton/homehub-cli/service"
+import (
+	"errors"
+
+	"github.com/jamesnetherton/homehub-cli/service"
+)
 
 // NewLightEnableCommand creates a new command to invoke the Hub LightEnable function
 func NewLightEnableCommand(authenticatingCommand *GenericCommand) *AuthenticationRequiringCommand {
@@ -8,8 +12,16 @@ func NewLightEnableCommand(authenticatingCommand *GenericCommand) *Authenticatio
 		GenericCommand: GenericCommand{
 			Name:        "LightEnable",
 			Description: "Toggles the Home Hub LED on or off",
+			ArgNames:    []string{"enable"},
+			ArgTypes:    []string{"bool"},
 			Exec: func(context *CommandContext) {
-				enable, _ := context.GetBooleanArg(0)
+				enable, err := context.GetBooleanArg(0)
+				if err != nil {
+					parseErr := errors.New("LightEnable must be either true or false")
+					context.SetResult(nil, parseErr)
+					return
+				}
+
 				context.SetResult(nil, service.GetHub().LightEnable(enable))
 			},
 		},
